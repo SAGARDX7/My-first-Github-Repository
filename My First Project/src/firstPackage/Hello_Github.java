1642fd58134b72289fe1e70707e519ad296a2428
SAGARDX7 <sagarsuman160492@gmail.com>
2019-02-08 00:19:41 +0530

    My first commit!

---
+++ My First Project/src/firstPackage/Hello_Github.java
@@ -0,0 +1,11 @@
+package firstPackage;
+
+public class Hello_Github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Github! First Project Sync-up");
+	}
+
+}
